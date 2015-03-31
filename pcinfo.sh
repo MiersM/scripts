7e21c502edaa9ac5d822c5994ef4cc4d24874cb2
smeggie <maartenmiers@gmail.com>
2015-03-31 19:13:24 +0200

    v2

--- pcinfo.sh
+++ pcinfo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This script prints the following: CPUmodel, used ram, total ram, % used ram,
-#IP address and default gateway.
+#hostname, IP address and default gateway.
 
 CPU=`cat /proc/cpuinfo | grep "model name" | awk -F: '{print $2}'`
 
@@ -13,6 +13,7 @@ mempct=`free | grep "Mem" | awk '{print (($3/$2)*100)}'| awk '{print int($1+0.5)
 IP=`hostname -I`
 DG=`route | grep "default" | awk '{print $2}'`
 
+echo "Your hostname is: `hostname`"
 echo "Your CPU model is:$CPU"
 echo -e "Your used memory is: \e[4m$memused MB\e[24m, this is \e[4m$mempct%\e[24m of your total memory: \e[4m$memtotal MB\e[24m"
 echo -n "Your IP address is: $IP"; echo " Your Default Gateway is: $DG"
